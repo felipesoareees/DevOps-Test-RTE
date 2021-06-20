4bf6d4dff019c106d24c282cdbeffa8d136cf289
Felipe <feehsuarez@gmail.com>
2021-06-20 00:40:14 -0300

    alterando docker file 01

--- scripts/commitReport.sh
+++ scripts/commitReport.sh
@@ -4,4 +4,4 @@
 #DATA: 18/06/2021								     #
 #DESCRIÇÃO: ENVIA UM ALERTA INFORMANDO QUE O PROJETO DevOps-Test-RTE FOI ALTERADO ####
 
-curl -m 5 -X POST -H 'Content-Type: application/json' -d '{"chat_id": "-1001243857260","text": "FOI REALIZADO UM NOVO COMMIT NO PROJETO DEVOPS-TEST-RTE. VOCÊ PODE OPTAR POR EXECUTAR A JOB ANSIBLE_NGINX. BASTA ACESSAR O JENKINS NO ENDEREÇO: http://18.116.71.153:8080", "disable_notification": false}' https://api.telegram.org/bot637003638:AAGcQQaW2UPLS7DN8iBMbreEIHIfp7OU57Q/sendMessage
+curl -m 5 -X POST -H 'Content-Type: application/json' -d '{"chat_id": "-1001243857260","text": "FOI REALIZADO UM NOVO COMMIT NO PROJETO DEVOPS-TEST-RTE. VOCÊ PODE OPTAR POR EXECUTAR A JOB ANSIBLE_NGINX. BASTA ACESSAR O JENKINS NO ENDEREÇO: http://18.191.211.184:8080", "disable_notification": false}' https://api.telegram.org/bot637003638:AAGcQQaW2UPLS7DN8iBMbreEIHIfp7OU57Q/sendMessage
